b451ea4ee38235b0dfa20edcb8f1ae8ac5b33b8c
unknown <singhakritarth@gmail.com>
2023-05-15 19:40:47 +0530

    OK

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="bg-black bg-image px-10 h-screen w-full justify-center items-center flex relative">
       {/* <img src="/assets/bh_white.png" className="absolute opacity-20" /> */}
       <div className="flex flex-col">
-        <span className="pb-4 gradient-text text-8xl font-bold text-center tracking-[-7px]">
+        <span className="pb-4 gradient-text md:text-8xl  font-bold text-center md:tracking-[-7px] text-6xl tracking-[-3px]">
           Get Ready for a Spectacular Arrival!
         </span>
         <div className="flex flex-row items-center justify-center"></div>
